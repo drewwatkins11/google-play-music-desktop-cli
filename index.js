41b1d1a17e579cdb7014aa012ea96587e6d2172f
Drew Watkins <dwatkins@castandhue.com>
2017-04-08 16:59:14 -0700

    Refactored to single function

--- index.js
+++ index.js
@@ -7,63 +7,39 @@ const chalk = require('chalk');
 var config = require('./config.js');
 
 const ws = new WebSocket(config.url);
+const appname = config.appname;
+const secret = config.secret;
 
-
-
-// single function - send both namespace and method as a result of option
-// incorporate ES6 literals for secret
-
-function gPlay(){
+function wsSend(namespace,method){
   ws.on('open', function open() {
-    ws.send('{"namespace": "connect", "method": "connect", "arguments": ["goom-cli","a4459902-f302-4054-8ccf-94e6ed83ae9a"]}');
-    console.log(chalk.green(`Connected to Google Play Music Desktop`));
-    const gplay = tryEval('{"namespace": "playback",  "method": "playPause"}');
-    ws.send(gplay);
+    ws.send(`{"namespace": "connect", "method": "connect", "arguments": ["${appname}","${secret}"]}`);
+    let gCommand = tryEval(`{"namespace": "${namespace}",  "method": "${method}"}`);
+    // checkRating();
+    ws.send(gCommand);
+    // checkTrack();
+    consoleOutput(method);
+    ws.close();
     ws.terminate();
-  });
+  })
 };
 
-function gNext(){
-  ws.on('open', function open() {
-    ws.send('{"namespace": "connect", "method": "connect", "arguments": ["goom-cli","a4459902-f302-4054-8ccf-94e6ed83ae9a"]}');
-    console.log(chalk.green(`Connected to Google Play Music Desktop`));
-    const gnext = tryEval('{"namespace": "playback",  "method": "forward"}');
-    ws.send(gnext);
-    ws.terminate();
-  });
-};
+function consoleOutput(method){
+  // check if currently playing track
+  // playback.getPlaybackState()
+  if (method == "playPause"){
+    console.log(chalk.green(`Playback has been toggled`));
+  } else if (method == "forward"){
+    console.log(chalk.green(`Skipping to next track`));
+  } else if (method == "rewind"){
+    console.log(chalk.green(`Skipping to previous track`));
+  } else if (method == "toggleThumbsUp"){
+    console.log(chalk.green(`The track has been up-voted`));
+  } else if (method == "toggleThumbsDown"){
+    console.log(chalk.green(`The track has been down-voted`));
+  }
+}
 
-function gPrev(){
-  ws.on('open', function open() {
-    ws.send('{"namespace": "connect", "method": "connect", "arguments": ["goom-cli","a4459902-f302-4054-8ccf-94e6ed83ae9a"]}');
-    console.log(chalk.green(`Connected to Google Play Music Desktop`));
-    const gprev = tryEval('{"namespace": "playback",  "method": "rewind"}');
-    ws.send(gprev);
-    ws.terminate();
-  });
-};
-
-function gRateUp(){
-  ws.on('open', function open() {
-    ws.send('{"namespace": "connect", "method": "connect", "arguments": ["goom-cli","a4459902-f302-4054-8ccf-94e6ed83ae9a"]}');
-    console.log(chalk.green(`Connected to Google Play Music Desktop`));
-    const grateup = tryEval('{"namespace": "rating",  "method": "toggleThumbsUp"}');
-    ws.send(grateup);
-    ws.terminate();
-  });
-};
-
-function gRateDown(){
-  ws.on('open', function open() {
-    ws.send('{"namespace": "connect", "method": "connect", "arguments": ["goom-cli","a4459902-f302-4054-8ccf-94e6ed83ae9a"]}');
-    console.log(chalk.green(`Connected to Google Play Music Desktop`));
-    const gratedown = tryEval('{"namespace": "rating",  "method": "toggleThumbsDown"}');
-    ws.send(gratedown);
-    ws.terminate();
-  });
-};
-
-function tryEval(str) {
+function tryEval(str){
   try {
     return JSON.stringify(eval(`(${str})`));
   } catch (e) {
@@ -71,28 +47,40 @@ function tryEval(str) {
   }
 }
 
-program.command('play')
- .description('toggle play/pause')
- .action(gPlay);
+ program.command('play')
+  .description('toggle play/pause')
+  .action(function(){
+    wsSend('playback','playPause');
+  });
 
 program.command('pause')
   .description('toggle play/pause')
-  .action(gPlay);
+  .action(function(){
+    wsSend('playback','playPause');
+  });
 
 program.command('prev')
- .description('toggle play/pause')
- .action(gPrev);
+ .description('previous track')
+ .action(function(){
+   wsSend('playback','rewind');
+ });
 
 program.command('next')
-.description('toggle play/pause')
-.action(gNext);
+.description('next track')
+.action(function(){
+  wsSend('playback','forward');
+});
 
 program.command('down')
- .description('toggle play/pause')
- .action(gRateDown);
+ .description('toggle thumbs-down rating')
+ .action(function(){
+   wsSend('rating','toggleThumbsDown');
+ });
 
 program.command('up')
-  .description('toggle play/pause')
-  .action(gRateUp);
+  .description('toggle thumbs-up rating')
+  .action(function(){
+    wsSend('rating','toggleThumbsUp');
+  });
 
 program.parse(process.argv);
